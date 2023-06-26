aad29b35a947e77e99d654bd47da333f1173e821
Xoerix <ampheriz0@gmail.com>
2023-06-26 03:06:30 -0600

    o

--- Pruebas/Prueba1/axsd.asm
+++ Pruebas/Prueba1/axsd.asm
@@ -1,60 +1,16 @@
 section .data
-
-msg db "Hola Mundo!", 0x0a, 0x0d
-len equ $-msg
+msg db "Bienvenido", 0x0a, 0x0d
+len equ $ - msg
 
 section .text
     global _start
 _start:
 
-mov eax,4
-mov ebx,1
-mov ecx,msg
+mov eax, 4
+mov ebx, 1
+mov ecx, msg
 mov edx, len
-
 int 0x80
 
 mov eax, 1
 int 0x80
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                      
